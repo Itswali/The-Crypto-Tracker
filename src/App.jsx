87aaca50eebd52179952513fd5ad3a276cf3e7a5
Itswali <luckywali0986@gmail.com>
2026-02-11 17:06:03 +0500

    setup react tailwind for the project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+  <div>
+
+    <div className="bg-black ring-2 ring-blue-500 text-5xl text-amber-600">Hello World!</div>
+
+  </div>
+  )
+}
+
+export default App
